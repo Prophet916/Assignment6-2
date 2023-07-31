8010054a66c05e1a995fe66b1e211e78d3e044fb
tach9 <tach9@DESKTOP-1AEJVRA>
2023-07-30 19:03:22 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Write a C# program to implement a stack by using array with push and pop operations. 
+             * Find the top element of the stack and check if the stack is empty or not.
+             */
+
+            /*
+             * Use the above implemented stack to search for any element
+             */
+
+            int stackSize = 5;
+            Stacks stack = new Stacks(stackSize);
+
+            Console.WriteLine("Is stack empty? " + stack.IsEmpty());
+
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+            stack.Push(40);
+            stack.Push(50);
+
+            Console.WriteLine("Is stack empty? " + stack.IsEmpty());
+            Console.WriteLine("Is stack full? " + stack.IsFull());
+
+            int topElement = stack.Peek();
+            Console.WriteLine("Top element: " + topElement);
+
+            int searchElement = 30;
+            stack.Search(searchElement);
+
+            searchElement = 60;
+            stack.Search(searchElement);
+
+            stack.Pop();
+            stack.Pop();
+            stack.Pop();
+
+            Console.WriteLine("Is stack empty? " + stack.IsEmpty());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Stacks.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6_2
+{
+    internal class Stacks
+    {
+        private int[] arr;
+        private int top;
+        private int capacity;
+
+        public Stacks(int size)
+        {
+            capacity = size;
+            arr = new int[capacity];
+            top = -1;
+        }
+
+        public bool IsEmpty()
+        {
+            return top == -1;
+        }
+
+        public bool IsFull()
+        {
+            return top == capacity - 1;
+        }
+
+        public void Push(int value)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Stack is full. Cannot push element.");
+                return;
+            }
+
+            top++;
+            arr[top] = value;
+            Console.WriteLine("Pushed: " + value);
+        }
+
+        public int Pop()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack is empty. Cannot pop element.");
+                return -1;
+            }
+
+            int value = arr[top];
+            top--;
+            Console.WriteLine("Popped: " + value);
+            return value;
+        }
+
+        public int Peek()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack is empty. Cannot peek element.");
+                return -1;
+            }
+
+            return arr[top];
+        }
+
+        public bool Search(int value)
+        {
+            int originalTop = top;
+            while (top >= 0)
+            {
+                if (arr[top] == value)
+                {
+                    Console.WriteLine("Element " + value + " found at index " + top);
+                    return true;
+                }
+                top--;
+            }
+
+            Console.WriteLine("Element " + value + " not found in the stack.");
+            top = originalTop; // Restore the original top value after the search
+            return false;
+        }
+    }
+}
